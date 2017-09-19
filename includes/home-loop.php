b1cdc980fb5ed99c2d0abd4b13585ed10026035d
Jamie Fraser <jamie.fraser@august.com.au>
2017-09-19 19:58:19 +0100

    next round of mini-changes

--- includes/home-loop.php
+++ includes/home-loop.php
@@ -7,20 +7,22 @@ if (!$thumb) {
 	);
 }
 ?>
-<div class="col s8 offset-s2 m4">
+<div class="col s12 m4">
 	<div class="card small">
 		<div class="card-image waves-effect waves-block waves-blue">
 			<?php echo $thumb; ?>
 		</div>
-		<div class="card-content">
-			<span class="card-title activator grey-text text-darken-4"><?php echo get_the_title(); ?><i
-					class="material-icons right">more_vert</i></span>
-			<p><a href="<?php echo get_the_permalink(); ?>">Read Post</a></p>
+		<div class="card-stacked">
+			<div class="card-content">
+				<span class="card-title activator grey-text text-darken-4"><i class="material-icons right" aria-hidden="true">more_vert</i> <?php echo get_the_title(); ?></span>
+			</div>
+			<div class="card-action single-card-action">
+				<a href="<?php the_permalink(); ?>" class="block blue-text">Read Post <i class="material-icons right" aria-hidden="true">arrow_forward</i></a>
+			</div>
 		</div>
 		<div class="card-reveal">
-			<span class="card-title grey-text text-darken-4"><?php echo get_the_title(); ?><i
-					class="material-icons right">close</i></span>
-			<p><?php echo get_the_excerpt(); ?></p>
+			<i class="material-icons right">close</i>
+			<?php echo wpautop(get_the_excerpt()); ?>
 			<div class="reveal-foot">
 				<?php echo get_the_tag_list( '<div class="chip">#', '</div><div class="chip">#', '</div>', get_the_ID() ); ?>
 			</div>
